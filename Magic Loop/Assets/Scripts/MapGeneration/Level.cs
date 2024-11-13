877b7c7f3965a21fe602e2072c56a1987061f869
Pol Robledillo <pol.robledillo.7e7@itb.cat>
2024-11-13 20:00:40 +0100

    added movement

---
+++ Magic Loop/Assets/Scripts/MapGeneration/Level.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Level
+{
+    public static float width = 10;
+    public static float height = 10;
+    public static float scale = 1;
+    public static Sprite Default, Unexplored, Current, Shop, Boss;
+}
